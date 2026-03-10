9a8b02cf8a0b6f708aadb0c6864ca930740fbfb4
khiem <khiemnguyen.hye@gmail.com>
2026-03-10 18:51:35 +0700

    Update monitor serv link

--- config/services.ts
+++ config/services.ts
@@ -2,7 +2,7 @@ export const MONITORED_SERVICES = [
   { name: "KatanaID",     url: "https://katanaid.com" },
   { name: "KatanaID API", url: "https://api.katanaid.com" },
   { name: "Caphne",       url: "https://caphne.co" },
-  { name: "Caphne API",   url: "https://api.caphne.com" },
+  { name: "Caphne API",   url: "https://api.caphne.co" },
   { name: "Anyu",         url: "https://anyu.sukaseven.com" },
   { name: "Tldraw",       url: "https://tldraw.sukaseven.com" },
   { name: "Hasaki",       url: "https://hasaki.sukaseven.com" },
